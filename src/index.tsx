691f40c1b756e80eb502be0eea2b2606ffef869d
Hofund-pro <hofund.pro@gmail.com>
2022-07-26 20:56:33 -0300

    First Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './main.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Backdrop, CircularProgress} from "@mui/material";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Suspense fallback={<Backdrop sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}} open>
+      <CircularProgress color="inherit"/>
+    </Backdrop>}>
+      <Routes>
+        <Route path={'/'} element={<App/>}>
+
+        </Route>
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
